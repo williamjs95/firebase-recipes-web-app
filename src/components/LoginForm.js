861b2147d1533cb4fe79abfd69aa345deac26606
williamjs95 <williamj.silva@live.com>
2024-11-05 19:42:49 -0300

    Implement Firebase authentication with login and registration features

---
+++ src/components/LoginForm.js
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import FirebaseAuthService from "../FirebaseAuthService";
+
+function LoginForm({ existingUser }) {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      await FirebaseAuthService.loginUser(username, password);
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  async function handleSendResetPasswordEmail() {
+    if (!username) {
+      alert('Missing username!');
+      return;
+    }
+
+    try {
+      await FirebaseAuthService.sendPasswordResetEmail(username);
+      alert('Password reset email sent!');
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  async function handleLoginWithGoogle() {
+    try {
+      await FirebaseAuthService.loginWithGoogle();
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  function handleLogout() {
+    FirebaseAuthService.logoutUser();
+  }
+
+  return (
+    <div className="login-form-container">
+      {
+        existingUser ? (
+          <div className="row">
+            <h3>Welcome, {existingUser.email}</h3>
+            <button type="button" className="primary-button" onClick={handleLogout}>
+              Logout
+            </button>
+        </div> 
+      ) : (
+        <form onSubmit={handleSubmit} className="login-form">
+          <label className="input-label login-label">
+            Username (email):
+            <input
+              type="email"
+              required
+              value={username}
+              onChange={ (e) => setUsername(e.target.value) }
+              className="input-text"
+            />
+          </label>
+          <label className="input-label login-label">
+            Password:
+            <input
+              type="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input-text"
+            />
+          </label>
+          <div className="button-box">
+            <button className="primary-button">
+              Login
+            </button>
+            <button type="button" onClick={handleSendResetPasswordEmail} className="primary-button">
+              Reset Password
+            </button>
+            <button type="button" onClick={handleLoginWithGoogle} className="primary-button">
+              Login with Google
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
+  )
+} 
+
+export default LoginForm;
