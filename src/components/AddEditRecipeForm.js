afe8c6bdb2b519ea199e348ca3dc65ab28922061
williamjs95 <williamj.silva@live.com>
2024-11-06 21:57:25 -0300

    Implement recipe form submission and ingredient management features

--- src/components/AddEditRecipeForm.js
+++ src/components/AddEditRecipeForm.js
@@ -10,8 +10,47 @@ function AddEditRecipeForm({ handleAddRecipe }) {
   const [ingredients, setIngredients] = useState([])
   const [ingredientName, setIngredientName] = useState('')
 
+  function handleRecipeFormSubmit(e) {
+    e.preventDefault()
+
+    if (ingredients.length === 0) {
+      alert('Ingredients cannot be empty. Please add at least one ingredient.')
+      return
+    }
+
+    const isPublished = new Date(publishDate) <= new Date() ? true : false
+
+    const newRecipe = {
+      name,
+      category,
+      directions,
+      publishDate: new Date(publishDate),
+      isPublished,
+      ingredients
+    }
+
+    handleAddRecipe(newRecipe)
+  }
+
+  function handleAddIngredient(e) {
+    if (e.key && e.key !== 'Enter') {
+      return
+    }
+
+    e.preventDefault()
+
+    if (!ingredientName) {
+      alert('Missing ingredient')
+      return
+    }
+
+    setIngredients([...ingredients, ingredientName])
+    setIngredientName('')
+  }
+
   return (
-    <form className="add-edit-recipe-form-container">
+    <form 
+      onSubmit={handleRecipeFormSubmit} className="add-edit-recipe-form-container">
       <h2>Add a New Recipe</h2>
       <div className="top-form-section">
         <div className="fields">
@@ -70,6 +109,71 @@ function AddEditRecipeForm({ handleAddRecipe }) {
             />
           </label>
         </div>
+      </div>
+      <div className="ingredients-list">
+        <h3 className="text-center">Ingredients</h3>
+        <table className="ingredients-table">
+          <thead>
+            <tr>
+              <th className="table-header">Ingredient</th>
+              <th className="table-header">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              ingredients && ingredients.length > 0 ? ingredients.map((ingredient) => {
+                return (
+                  <tr key={ingredient}>
+                    <td className="table-data text-center">
+                      {ingredient}
+                    </td>
+                    <td className="ingredient-delete-box">
+                      <button 
+                        type="button" className="secondary-button ingredient-delete-button"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                )
+              }) : null
+            }
+          </tbody>
+        </table>
+        {
+          ingredients && ingredients.length == 0 ? 
+          <h3 className="text-center no-ingredients">
+            No Ingredients Added Yet
+          </h3> : null
+        }
+        <div className="ingredient-form">
+          <label className="ingredient-label">
+            Ingredient:
+            <input 
+              type="text" 
+              value={ingredientName}
+              onChange={(e) => setIngredientName(e.target.value)}
+              onKeyPress={handleAddIngredient}
+              className="input-text"
+              placeholder="ex. 1 cup of sugar"
+            />
+          </label>
+          <button 
+            type="button"
+            className="primary-button add-ingredient-button"
+            onClick={handleAddIngredient}
+          >
+            Add Ingredient
+          </button>
+        </div>
+      </div>
+      <div className="action-buttons">
+        <button 
+          type="submit" 
+          className="primary-button action-button"
+        >
+          Create Recipe
+        </button>
       </div>
     </form>
   )
